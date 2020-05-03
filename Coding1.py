a7efad0c913a5249147ef8967a5b187e59245b32
17341 <17341@ecam.be>
2020-05-03 02:01:26 +0200

    0.9.0

---
+++ Coding1.py
@@ -0,0 +1,71 @@
+import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2, sys, os
+
+# Transforme l'image en grayscale:
+
+picture = sys.argv[2]
+im1 = mpimg.imread(picture)
+img1 = np.copy(im1)                                             #On fait une copie de l'original
+print(img1.shape)                                               #Donne le nbr de pixel de l'image , le troisieme nombre est le nombre de couleur 3(rouge,vert,bleu).
+print(img1)                                                     #Donne un tableau avec les valeurs rgb de chaque pixel, img1[ligne][colonne]
+
+#With matplotlib
+
+def color_matplot():
+    c =  sys.argv[3]    
+    rgb= np.array([0.2989, 0.5870, 0.1140])                     #Les constantes utilisé pour l'intensité effectif de luminosité d'un pixel.
+    color_image = np.dot(img1, rgb)                             #On multiplie chaque valeur rgb de chaque pixel par l'intensité effectif.
+    plt.imshow(np.flipud(color_image), cmap=plt.get_cmap(c))    #Finalement on fait une image de ça avec la couleur gris.
+    plt.show()
+    print(color_image)
+
+def darker_matplot():
+    b =  sys.argv[3]
+    for i in range(img1.shape[0]):
+        for j in range(img1.shape[1]):
+            img1[i,j] = np.divide(img1[i,j],int(b))             #On divise chaque valeur rgb de chaque pixel par 2.                               
+    plt.imshow(img1)       
+    plt.show()
+    
+def invers_matplot():
+    fig, axes = plt.subplots(2) 
+    axes[0].imshow(img1)
+    axes[1].imshow(np.flipud(img1))  
+    fig.set_size_inches(10, 7)                                  #Flipud , flip verticalement
+    plt.show()
+
+#With Pillow
+
+def gray_pillow():
+    im2 = np.array(Image.open('Picture.jpg').convert('L'))
+    img2 = Image.fromarray(im2)
+    img2.show()
+
+#With OpenCV
+
+def gray_cv2():
+    image = cv2.imread('Picture.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Gray image', gray)
+    cv2.waitKey(0)
+
+
+try:
+    if sys.argv[1] == "color-matplot" :
+        color_matplot()
+    if sys.argv[1] == "darker-matplot" :
+        darker_matplot()
+    if sys.argv[1] == "gray-pillow" :
+        gray_pillow()
+    if sys.argv[1] == "gray-cv2":
+        gray_cv2()
+    if sys.argv[1] == "invers-matplot":
+        invers_matplot()
+    else : 
+        print("Programm not found, try with : color-matplot;gray-pillow;gray-cv2;invers-matplot;darker-matplot")
+except : 
+    print("Please type a programm name to open your file")
+
