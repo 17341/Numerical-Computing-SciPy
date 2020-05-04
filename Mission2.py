9e99f6af0d654e0c1b3c8e775fe293f9eb56b4bf
17341 <17341@ecam.be>
2020-05-04 20:45:24 +0200

    Final

--- Mission2.py
+++ Mission2.py
@@ -19,26 +19,26 @@ liste1 = []
 liste2 = []
 #On analyse le dictionnaire pour retirer les chiffres et on les stock dans une liste:
 for key in sacs.keys():                                                         
-    liste1.append(re.search('\d+', key).group())
+    liste1.append(re.search(r'\d+', key).group())
     for n in range(len(sacs[key])):
         if o.match(sacs[key][n]) is not None:
-            liste2.append(re.search('\d+', sacs[key][n]).group())
+            liste2.append(re.search(r'\d+', sacs[key][n]).group())
         if f.match(sacs[key][n]) is not None:
-            liste2.append(re.search('\d+', sacs[key][n]).group()) 
+            liste2.append(re.search(r'\d+', sacs[key][n]).group()) 
         if b.match(sacs[key][n]) is not None:
-            liste2.append(re.search('\d+', sacs[key][n]).group())
+            liste2.append(re.search(r'\d+', sacs[key][n]).group())
 #On met les chiffres dans des tableaux en int:
 liste1 = np.array(liste1).astype("int64")                                       
 liste2 = np.reshape(np.array(liste2),(len(sacs.values()),3)).astype("int64")    
-#Solution la plus rapide et la plus efficace:
+#Solution la plus rapide et la plus efficace, on obtient une laste avec les résultats dans l'ordre:
 start1_time = time.time()                                                       
 result1 = np.linalg.solve(liste2,liste1)         
 print(result1,time.time() - start1_time)
-#Bonne solution:
+#Bonne solutionon, obtient une laste avec les résultats dans l'ordre:
 start2_time = time.time()                                                       
 result2 = np.linalg.inv(liste2).dot(liste1)
 print(result2,time.time() - start2_time)
-#Solution "plus longue" et moins efficace que les 2 autres.
+#Solution "plus longue" et moins efficace que les 2 autres, on obtient un tableau (1,3) avec les résultats dans l'ordre:
 start3_time = time.time()                                                       
 matrice = np.matrix(liste2)
 result3 = np.dot(matrice.I,liste1)
